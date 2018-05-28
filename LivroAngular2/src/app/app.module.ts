d6c6e2f7fd654611168379453ad6454fdb454292
Karen Vicente <karenyasmin37@gmail.com>
2018-05-28 19:27:47 -0300

    add component interpolation binding

--- LivroAngular2/src/app/app.module.ts
+++ LivroAngular2/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 
 import { AlertaService } from './alerta.service';
+import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaPessoaComponent
+    ListaPessoaComponent,
+    InterpolationBindingComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
