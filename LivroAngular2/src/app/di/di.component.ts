63abf5c154da1f93041b134e37d6abb3b9ee2452
Karen Vicente <karenyasmin37@gmail.com>
2018-06-13 18:46:15 -0300

    add componente di

--- LivroAngular2/src/app/di/di.component.ts
+++ LivroAngular2/src/app/di/di.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NomesTecService } from './nomes-tec.service';
 import { MeuLogService } from './meu-log.service';
+import { AlertaService } from '../alerta.service';
 
 @Component({
   selector: 'app-di',
@@ -12,8 +13,9 @@ export class DiComponent implements OnInit {
 
   tecnologias: string[] = [];
 
-  constructor(private meuService: NomesTecService) {
+  constructor(private meuService: NomesTecService, private meuAlerta: AlertaService) {
     this.tecnologias = meuService.getNomesTec();
+    this.meuAlerta.msgAlerta();
   }
 
   ngOnInit() {
